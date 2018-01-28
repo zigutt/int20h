632ae4ff48eec706686be56148da9da236c7928f
Dmitriy Nickolay Petruk <mistake952@mail.ru>
2018-01-28 16:40:06 +0200

    webstorm using

--- server.js
+++ server.js
@@ -1,26 +1,24 @@
-const express = require('express')
-const app = express()
-var https = require('https');
+const express = require('express');
+const app = express();
+const https = require('https');
+
 app.set('view engine', 'ejs');
 let krakenCurr = [];
-let krakenFee = 0;
 let bittrexCurr = [];
-let bittrexFee = 0;
 let exmoCurr = [];
-let exmoFee = 0;
 let bitcoinCurrency;
 
 
 function updateKraken()
 {
 https.get('https://api.kraken.com/0/public/Ticker?pair=XBTUSD', res => {
-		  res.setEncoding("utf8");
+		//  res.setEncoding("utf8");
 		  let body = "";
 		  res.on("data", data => {
 		    body += data;
 		  });
 		  res.on("end", () => {
-		    var data = JSON.parse(body);
+		    let data = JSON.parse(body);
 		    krakenCurr = [ data.result['XXBTZUSD'].a[0], 
 		    				data.result['XXBTZUSD'].b[0] ];
 		    console.log("Loaded kraken");
@@ -30,14 +28,14 @@ https.get('https://api.kraken.com/0/public/Ticker?pair=XBTUSD', res => {
 function updateChart()
 {
 	https.get('https://api.blockchain.info/charts/market-price?timespan=30days&format=json', res => {
-		  res.setEncoding("utf8");
+		//  res.setEncoding("utf8");
 		  let body = "";
 		  res.on("data", data => {
 		    body += data;
 		//   	console.log(body);
 		  });
 		  res.on("end", () => {
-		    var data = JSON.parse(body);
+		    let data = JSON.parse(body);
 		    bitcoinCurrency = data.values;
 		    console.log("Loaded chart");
 		  });
@@ -45,19 +43,19 @@ function updateChart()
 }
 setInterval(updateChart, 10000);
 
-function updateBitTrex()
+function updateBittrex()
 {
 https.get('https://bittrex.com/api/v1.1/public/getticker?market=USDT-BTC', res => {
-		  res.setEncoding("utf8");
+		//  res.setEncoding("utf8");
 		  let body = "";
 		  res.on("data", data => {
 		    body += data;
 		   // console.log(body);
 		  });
 		  res.on("end", () => {
-		    var data = JSON.parse(body);
-		    bittrexCurr = [ data.result.Ask, 
-		    				data.result.Bid ];
+		    let data = JSON.parse(body);
+		    bittrexCurr = [ data.result['Ask'],
+		    				data.result['Bid'] ];
 		    console.log("Loaded bittrex");
 		  });
 		});
@@ -65,27 +63,28 @@ https.get('https://bittrex.com/api/v1.1/public/getticker?market=USDT-BTC', res =
 function updateExmo()
 {
 https.get('https://api.exmo.com/v1/ticker/', res => {
-		  res.setEncoding("utf8");
+		//  res.setEncoding("utf8");
 		  let body = "";
 		  res.on("data", data => {
 		    body += data;
 		   // console.log(body);
 		  });
 		  res.on("end", () => {
-		    var data = JSON.parse(body);
-		    exmoCurr = [ data['BTC_USD'].sell_price, 
-		    				data['BTC_USD'].buy_price ];
-		    console.log("Loaded exmo");
+		    let data = JSON.parse(body);
+		    exmoCurr = [ data['BTC_USD']['buy_price'],
+		    				data['BTC_USD']['sell_price'] ];
+		    console.log(`Loaded exmo`);
 		  });
 		});
 }
 setInterval(updateKraken, 10000);
-setInterval(updateBitTrex, 10000);
+setInterval(updateBittrex, 10000);
 setInterval(updateExmo, 10000);
+
 app.get('/', function (req, res) {
     res.render('index', {krakencurr : krakenCurr, bittrexcurr : bittrexCurr , exmocurr:exmoCurr, chart : bitcoinCurrency });
-})
+});
 
 app.listen(8080, function () {
-  console.log('Website runnning on 8080 port!');
-})
+  console.log('Website running on 8080 port!');
+});
